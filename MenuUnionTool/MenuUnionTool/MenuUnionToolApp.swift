0873dd24430d2fbc28cae89dbe89eb04c0dd331e
ShuheiIshihara <shuheinojobhunting@gmail.com>
2025-01-13 23:41:12 +0900

    Swiftプロジェクトを追加

---
+++ MenuUnionTool/MenuUnionTool/MenuUnionToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuUnionToolApp.swift
+//  MenuUnionTool
+//
+//  Created by 石原脩平 on 2025/01/13.
+//
+
+import SwiftUI
+
+@main
+struct MenuUnionToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

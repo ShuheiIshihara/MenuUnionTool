ec4395d2f7675dea87ec9becd69a59cd27a4eb15
ShuheiIshihara <shuheinojobhunting@gmail.com>
2025-02-11 11:26:13 +0900

    Revert "Cloud Firestoreの接続確認"
    This reverts commit 91e7ebb71a916dc7bed749ac87f71402254702c6.

--- MenuUnionTool/MenuUnionTool/MenuUnionToolApp.swift
+++ MenuUnionTool/MenuUnionTool/MenuUnionToolApp.swift
@@ -6,30 +6,9 @@
 //
 
 import SwiftUI
-import FirebaseCore
-import FirebaseFirestore
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-      
-      FirebaseApp.configure()
-      let db = Firestore.firestore()
-
-      // コレクションを作成
-      db.collection("items").document("new-item3").setData([
-        "name": "新しいアイテム2",
-        "price": 1002,
-      ])
-      
-      return true
-  }
-}
 
 @main
 struct MenuUnionToolApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
